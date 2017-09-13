c087fbadc7386a29f8eae6c191d13f7f57a849fa
Victor Chen <vc8724@utexas.edu>
2017-09-13 11:56:16 -0600

    added phasedemo_vic

---
+++ PhaseDemo_vic/state1.js
@@ -0,0 +1,10 @@
+var demo = {
+// this 1st variable is the name of the game
+	state1: function(){}
+};
+
+demo.state1.prototype = {
+	preload: function(){},
+	create: function(){},
+	update:function(){}
+}
